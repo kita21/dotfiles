89c9b77fcd8a2b7d8af19c744e919bdf965e8f6c
kita21 <forsyuvikings@gmail.com>
2025-02-02 10:21:57 +0900

    Add: gcloud

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,3 +2,12 @@
 export PATH="$PATH:/opt/homebrew/bin/"
 # rancher desktop
 export PATH="~/.rd/bin:$PATH"
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+# gcloud
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
+# The next line enables shell command completion for gcloud.
+if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
