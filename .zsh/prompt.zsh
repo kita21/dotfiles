ae6c76504b962b8acbb49e6831db7b873e24a730
kita21 <forsyuvikings@gmail.com>
2025-02-03 20:47:57 +0900

    Update: prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -7,7 +7,7 @@ source /opt/homebrew/opt/kube-ps1/share/kube-ps1.sh
 update_git_info () {
   vcs_info
   # 左に実行ユーザとgitブランチ表示
-  PROMPT="%F{yellow}[%n]${vcs_info_msg_0_}$(kube_ps1)$ %f"
+  PROMPT="$(kube_ps1)%F{yellow}[%n]${vcs_info_msg_0_}$ %f"
 }
 add-zsh-hook precmd update_git_info
 
