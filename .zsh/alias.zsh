594d1eb177b1a0dfafb429ce1708059b9a2c145d
kita21 <forsyuvikings@gmail.com>
2025-06-20 22:24:23 +0900

    Update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,3 +1,5 @@
 alias ll='ls -alfh'
 alias k='kubectl'
+alias tf='terraform'
+alias cr='cursor'
 alias calx='cal -3; curl -s https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv | iconv -f SHIFT-JIS -t UTF-8 | grep -E "`date -v-1m '+%Y/%-m/'`|`date '+%Y/%-m/'`|`date -v+1m '+%Y/%-m/'`"'
