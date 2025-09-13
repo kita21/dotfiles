52fd974f0ce01a1a81d56769b456e1e71381442b
kita21 <forsyuvikings@gmail.com>
2025-09-14 00:13:37 +0900

    Feat: lsp terraformls

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -13,4 +13,4 @@ vim.diagnostic.config({
 
 local lua_ls_opts = require('lsp.lua_ls')
 vim.lsp.config('lua_ls', lua_ls_opts)
-vim.lsp.enable('lua_ls', 'ts_ls')
+vim.lsp.enable('lua_ls', 'ts_ls', 'terraformls')
