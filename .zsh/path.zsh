be3855fdb48e81c414c3441a78c9ca8882d6e22c
kita21 <forsyuvikings@gmail.com>
2025-09-18 19:08:41 +0900

    Feat: brew package

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,10 +2,8 @@
 export PATH="$PATH:/opt/homebrew/bin/"
 # rancher desktop
 export PATH="~/.rd/bin:$PATH"
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - zsh)"
+# mise
+eval "$(mise activate zsh)"
 # gcloud
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then . "${HOME}/google-cloud-sdk/path.zsh.inc"; fi
